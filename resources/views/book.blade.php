73b093e19c59087969cc2595d01982eee5ce2fc0
asaid-0 <60660214+asaid-0@users.noreply.github.com>
2020-03-11 12:30:19 -0500

    user books

---
+++ resources/views/book.blade.php
@@ -0,0 +1,305 @@
+<!doctype html>
+<!--[if lt IE 7]>		<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>			<html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>			<html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->	<html class="no-js" lang=""> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>BootStrap HTML5 CSS3 Theme</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/icomoon.css">
+	<link rel="stylesheet" href="css/jquery-ui.css">
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="stylesheet" href="css/transitions.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/color.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/slider.css">
+	<script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+</head>
+<body>
+	<!--[if lt IE 8]>
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+	<!--************************************
+			Wrapper Start
+	*************************************-->
+	<div id="tg-wrapper" class="tg-wrapper tg-haslayout">
+		<!--************************************
+				Header Start
+		*************************************-->
+		<header id="tg-header" class="tg-header tg-haslayout">
+			<div class="tg-topbar">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+							<ul class="tg-addnav">
+                                <li>
+									<a href="javascript:void(0);">
+										<i style="font-size: 4rem; color: #3e3e3e" class="icon-book"></i>
+									</a>
+								</li>
+								<li>
+									<a href="javascript:void(0);">
+										<i class="icon-home"></i>
+										<em>Home</em>
+									</a>
+								</li>
+								<li>
+									<a href="javascript:void(0);">
+										<i class="icon-right"></i>
+										<em>My Books</em>
+									</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);" id="tg-wishlisst" class="tg-btnthemedropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										<span class="tg-themebadge">3</span>
+										<i class="icon-heart"></i>
+										<span>Favorites</span>
+									</a>
+
+
+                                </li>
+							</ul>
+							<div class="tg-userlogin">
+								<figure><a href="javascript:void(0);"><img style="height: 30px" src="images/users/img-01.jpg" alt="user"></a></figure>
+								<span>Hi, User</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			
+		</header>
+        
+        
+
+
+
+
+
+
+
+
+<footer>
+        <div class="carousel-wrapper" id="products">
+            <ul class="carousel-inner clearfix">
+                <li class="item">
+                    <div class="col-md-12">
+                        <div class="col-md-4 image-container">
+                            <img src="images/books/img-01.jpg" alt="title" />
+                        </div>
+                        
+                        <div class="col-md-8 book-info">
+                            <div>
+                                <ul class="tg-bookscategories">
+                                    <li><a href="javascript:void(0);">Children’s Book</a></li>
+                                </ul>
+                            </div>
+                            
+                            <div class="tg-themetagbox"><span class="tg-themetag">5 copies</span></div>
+                            
+                            <div class="tg-booktitle">
+                                <h3><a href="javascript:void(0);">Help Me Find My Stomach</a></h3>
+                            </div>
+                            
+                            <div>
+                                <span class="tg-bookwriter">By: <a href="javascript:void(0);">Angela Gunning</a></span>
+                            </div>
+                            
+                            <div>
+                                <a class="tg-btn tg-btnstyletwo" href="javascript:void(0);">
+                                    <i class="fa fa-shopping-basket"></i>
+                                    <em>Lease</em>
+                                </a>
+                            </div>
+                            
+
+                            
+                        </div>
+                    </div>
+                </li>
+                <li class="item">
+                    <div class="col-md-12">
+                        <div class="col-md-4 image-container">
+                            <img src="images/books/img-01.jpg" alt="title" />
+                        </div>
+                        
+                        <div class="col-md-8 book-info">
+                            <div>
+                                <ul class="tg-bookscategories">
+                                    <li><a href="javascript:void(0);">Children’s Book</a></li>
+                                </ul>
+                            </div>
+                            
+                            <div class="tg-themetagbox"><span class="tg-themetag">5 copies</span></div>
+                            
+                            <div class="tg-booktitle">
+                                <h3><a href="javascript:void(0);">Help Me Find My Stomach</a></h3>
+                            </div>
+                            
+                            <div>
+                                <span class="tg-bookwriter">By: <a href="javascript:void(0);">Angela Gunning</a></span>
+                            </div>
+                            
+                            <div>
+                                <a class="tg-btn tg-btnstyletwo" href="javascript:void(0);">
+                                    <i class="fa fa-shopping-basket"></i>
+                                    <em>Lease</em>
+                                </a>
+                            </div>
+                            
+
+                            
+                        </div>
+                    </div>
+                </li>
+                <li class="item">
+                    <div class="col-md-12">
+                        <div class="col-md-4 image-container">
+                            <img src="images/books/img-01.jpg" alt="title" />
+                        </div>
+                        
+                        <div class="col-md-8 book-info">
+                            <div>
+                                <ul class="tg-bookscategories">
+                                    <li><a href="javascript:void(0);">Children’s Book</a></li>
+                                </ul>
+                            </div>
+                            
+                            <div class="tg-themetagbox"><span class="tg-themetag">5 copies</span></div>
+                            
+                            <div class="tg-booktitle">
+                                <h3><a href="javascript:void(0);">Help Me Find My Stomach</a></h3>
+                            </div>
+                            
+                            <div>
+                                <span class="tg-bookwriter">By: <a href="javascript:void(0);">Angela Gunning</a></span>
+                            </div>
+                            
+                            <div>
+                                <a class="tg-btn tg-btnstyletwo" href="javascript:void(0);">
+                                    <i class="fa fa-shopping-basket"></i>
+                                    <em>Lease</em>
+                                </a>
+                            </div>
+                            
+
+                            
+                        </div>
+                    </div>
+                </li>
+                <li class="item">
+                    <div class="col-md-12">
+                        <div class="col-md-4 image-container">
+                            <img src="images/books/img-01.jpg" alt="title" />
+                        </div>
+                        
+                        <div class="col-md-8 book-info">
+                            <div>
+                                <ul class="tg-bookscategories">
+                                    <li><a href="javascript:void(0);">Children’s Book</a></li>
+                                </ul>
+                            </div>
+                            
+                            <div class="tg-themetagbox"><span class="tg-themetag">5 copies</span></div>
+                            
+                            <div class="tg-booktitle">
+                                <h3><a href="javascript:void(0);">Help Me Find My Stomach</a></h3>
+                            </div>
+                            
+                            <div>
+                                <span class="tg-bookwriter">By: <a href="javascript:void(0);">Angela Gunning</a></span>
+                            </div>
+                            
+                            <div>
+                                <a class="tg-btn tg-btnstyletwo" href="javascript:void(0);">
+                                    <i class="fa fa-shopping-basket"></i>
+                                    <em>Lease</em>
+                                </a>
+                            </div>
+                            
+
+                            
+                        </div>
+                    </div>
+                </li>
+                <li class="item">
+                    <div class="col-md-12">
+                        <div class="col-md-4 image-container">
+                            <img src="images/books/img-01.jpg" alt="title" />
+                        </div>
+                        
+                        <div class="col-md-8 book-info">
+                            <div>
+                                <ul class="tg-bookscategories">
+                                    <li><a href="javascript:void(0);">Children’s Book</a></li>
+                                </ul>
+                            </div>
+                            
+                            <div class="tg-themetagbox"><span class="tg-themetag">5 copies</span></div>
+                            
+                            <div class="tg-booktitle">
+                                <h3><a href="javascript:void(0);">Help Me Find My Stomach</a></h3>
+                            </div>
+                            
+                            <div>
+                                <span class="tg-bookwriter">By: <a href="javascript:void(0);">Angela Gunning</a></span>
+                            </div>
+                            
+                            <div>
+                                <a class="tg-btn tg-btnstyletwo" href="javascript:void(0);">
+                                    <i class="fa fa-shopping-basket"></i>
+                                    <em>Lease</em>
+                                </a>
+                            </div>
+                            
+
+                            
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <a href="#" class="carousel-left">
+                <i class="fa fa-chevron-left slider-btn"></i>
+            </a>
+            <a href="#" class="carousel-right">
+                <i class="fa fa-chevron-right slider-btn"></i>
+            </a>
+          </div>
+
+
+
+
+        </footer>
+
+
+        
+          
+		
+	</div>
+	<!--************************************
+			Wrapper End
+	*************************************-->
+	<script src="js/vendor/jquery-library.js"></script>
+	<script src="https://maps.google.com/maps/api/js?key=AIzaSyCR-KEWAVCn52mSdeVeTqZjtqbmVJyfSus&language=en"></script>
+	{{-- <script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.vide.min.js"></script>
+	<script src="js/countdown.js"></script>
+	<script src="js/jquery-ui.js"></script>
+	<script src="js/parallax.js"></script>
+	<script src="js/countTo.js"></script>
+	<script src="js/appear.js"></script>
+	<script src="js/gmap3.js"></script>
+    <script src="js/main.js"></script> --}}
+    <script src="js/slider.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+</body>
+</html>
