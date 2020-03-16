553d10f0a7c42346ed8db82ac9612005511f5924
asaid-0 <60660214+asaid-0@users.noreply.github.com>
2020-03-16 06:31:46 -0500

    Fixed UI Paths

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -10,19 +10,19 @@
   <title>Admin page</title>
 
   <!-- Favicons -->
-  <link href="Dashio/img/favicon.png" rel="icon">
-  <link href="Dashio/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="/Dashio/img/favicon.png" rel="icon">
+  <link href="/Dashio/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Bootstrap core CSS -->
-  <link href="Dashio/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/Dashio/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <!--external css-->
-  <link href="Dashio/lib/font-awesome/css/font-awesome.css" rel="stylesheet" />
-  <link rel="stylesheet" type="text/css" href="Dashio/css/zabuto_calendar.css">
-  <link rel="stylesheet" type="text/css" href="Dashio/lib/gritter/css/jquery.gritter.css" />
+  <link href="/Dashio/lib/font-awesome/css/font-awesome.css" rel="stylesheet" />
+  <link rel="stylesheet" type="text/css" href="/Dashio/css/zabuto_calendar.css">
+  <link rel="stylesheet" type="text/css" href="/Dashio/lib/gritter/css/jquery.gritter.css" />
   <!-- Custom styles for this template -->
-  <link href="Dashio/css/style.css" rel="stylesheet">
-  <link href="Dashio/css/style-responsive.css" rel="stylesheet">
-  <script src="Dashio/lib/chart-master/Chart.js"></script>
+  <link href="/Dashio/css/style.css" rel="stylesheet">
+  <link href="/Dashio/css/style-responsive.css" rel="stylesheet">
+  <script src="/Dashio/lib/chart-master/Chart.js"></script>
 </head>
 
 <body>
@@ -170,16 +170,16 @@
     <!--footer end-->
   </section>
   <!-- js placed at the end of the document so the pages load faster -->
-  <script src="Dashio/lib/jquery/jquery.min.js"></script>
+  <script src="/Dashio/lib/jquery/jquery.min.js"></script>
 
-  <script src="Dashio/lib/bootstrap/js/bootstrap.min.js"></script>
-  <script class="include" type="text/javascript" src="Dashio/lib/jquery.dcjqaccordion.2.7.js"></script>
-  <script src="Dashio/lib/jquery.scrollTo.min.js"></script>
-  <script src="Dashio/lib/jquery.nicescroll.js" type="text/javascript"></script>
-  <script src="Dashio/lib/jquery.sparkline.js"></script>
+  <script src="/Dashio/lib/bootstrap/js/bootstrap.min.js"></script>
+  <script class="include" type="text/javascript" src="/Dashio/lib/jquery.dcjqaccordion.2.7.js"></script>
+  <script src="/Dashio/lib/jquery.scrollTo.min.js"></script>
+  <script src="/Dashio/lib/jquery.nicescroll.js" type="text/javascript"></script>
+  <script src="/Dashio/lib/jquery.sparkline.js"></script>
   <!--common script for all pages-->
-  <script src="Dashio/lib/common-scripts.js"></script>
-  <script type="text/javascript" src="Dashio/lib/gritter/js/jquery.gritter.js"></script>
-  <script type="text/javascript" src="Dashio/lib/gritter-conf.js"></script>
+  <script src="/Dashio/lib/common-scripts.js"></script>
+  <script type="text/javascript" src="/Dashio/lib/gritter/js/jquery.gritter.js"></script>
+  <script type="text/javascript" src="/Dashio/lib/gritter-conf.js"></script>
 </body>
 </html>
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -10,19 +10,19 @@
   <title>User page</title>
 
   <!-- Favicons -->
-  <link href="Dashio/img/favicon.png" rel="icon">
-  <link href="Dashio/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="/Dashio/img/favicon.png" rel="icon">
+  <link href="/Dashio/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Bootstrap core CSS -->
-  <link href="Dashio/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/Dashio/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <!--external css-->
-  <link href="Dashio/lib/font-awesome/css/font-awesome.css" rel="stylesheet" />
+  <link href="/Dashio/lib/font-awesome/css/font-awesome.css" rel="stylesheet" />
   <link rel="stylesheet" type="text/css" href="Dashio/css/zabuto_calendar.css">
   <link rel="stylesheet" type="text/css" href="Dashio/lib/gritter/css/jquery.gritter.css" />
   <!-- Custom styles for this template -->
-  <link href="Dashio/css/style.css" rel="stylesheet">
-  <link href="Dashio/css/style-responsive.css" rel="stylesheet">
-  <script src="Dashio/lib/chart-master/Chart.js"></script>
+  <link href="/Dashio/css/style.css" rel="stylesheet">
+  <link href="/Dashio/css/style-responsive.css" rel="stylesheet">
+  <script src="/Dashio/lib/chart-master/Chart.js"></script>
   <!---- bootstap and jquery---->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -173,16 +173,16 @@
     <!--footer end-->
   </section>
   <!-- js placed at the end of the document so the pages load faster -->
-  <script src="Dashio/lib/jquery/jquery.min.js"></script>
-  <script src="Dashio/lib/bootstrap/js/bootstrap.min.js"></script>
-  <script class="include" type="text/javascript" src="Dashio/lib/jquery.dcjqaccordion.2.7.js"></script>
-  <script src="Dashio/lib/jquery.scrollTo.min.js"></script>
-  <script src="Dashio/lib/jquery.nicescroll.js" type="text/javascript"></script>
-  <script src="Dashio/lib/jquery.sparkline.js"></script>
+  <script src="/Dashio/lib/jquery/jquery.min.js"></script>
+  <script src="/Dashio/lib/bootstrap/js/bootstrap.min.js"></script>
+  <script class="include" type="text/javascript" src="/Dashio/lib/jquery.dcjqaccordion.2.7.js"></script>
+  <script src="/Dashio/lib/jquery.scrollTo.min.js"></script>
+  <script src="/Dashio/lib/jquery.nicescroll.js" type="text/javascript"></script>
+  <script src="/Dashio/lib/jquery.sparkline.js"></script>
   <!--common script for all pages-->
-  <script src="Dashio/lib/common-scripts.js"></script>
-  <script type="text/javascript" src="Dashio/lib/gritter/js/jquery.gritter.js"></script>
-  <script type="text/javascript" src="Dashio/lib/gritter-conf.js"></script>
+  <script src="/Dashio/lib/common-scripts.js"></script>
+  <script type="text/javascript" src="/Dashio/lib/gritter/js/jquery.gritter.js"></script>
+  <script type="text/javascript" src="/Dashio/lib/gritter-conf.js"></script>
   <!-- javascript to active and deactive user-->
   <script src="/js/changeStatus.js"></script>
 </body>
