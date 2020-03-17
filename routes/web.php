783615bcce03931371bc38fe9b1ad4cee6d8fa2a
asaid-0 <60660214+asaid-0@users.noreply.github.com>
2020-03-17 13:10:06 -0500

    Solving conflicts in web.php and AdminController

--- routes/web.php
+++ routes/web.php
@@ -28,24 +28,21 @@ Route::get('/category/{cat}', array('uses' => 'User\BookController@list'));
 Route::get('/home', 'HomeController@index')->name('home');
 // Route::get('/admins', 'AdminController@admin')->name('admins');
 // Route::get('changeStatus', 'AdminController@ChangeUserStatus');
-Route::get('/users', 'AdminController@user')->name('users');
+// Route::get('/users', 'AdminController@user')->name('users');
 // Route::get('/books', 'AdminController@book')->name('books');
 Route::get('/categories', 'AdminController@category')->name('categories');
 Route::get('admins', 'AdminController@adminHome')->middleware('isAdmin')->name('admins');
-<<<<<<< HEAD
 Route::resource('users','AdminController') ;
 Route::resource('UserLeasedBook','UserLeasedBookController') ;
-Route::get('users', 'AdminController@index');
+// Route::get('users', 'AdminController@index');
 Route::get('/active_deactive_users/{id}', 'AdminController@active_deactive_users');
 Route::get('/search','User\BookController@search');
 Route::get('/showAdmins','AdminController@adminsPage')->name('showAdmins') ;
 
-=======
 Route::resource('books', 'BookController');
 Route::resource('categories', 'CategoryController');
 
 //Route::get('/test', function(){return view('test');});
->>>>>>> master
 
 Auth::routes();
 
